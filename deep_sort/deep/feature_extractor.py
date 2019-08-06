2103bb3a8510100a3db13c0031e7167d27c513b2
VietThang <vietthangtik15@gmail.com>
2019-08-06 15:44:58 +0700

    add patch net

--- deep_sort/deep/feature_extractor.py
+++ deep_sort/deep/feature_extractor.py
@@ -65,7 +65,7 @@ class Extractor(object):
             print(x, y, w, h)
             imgCrop = image[int(x):int(x+w), int(y):int(y+h)]
             print(imgCrop.shape)
-            cv2.imwrite("/home/vietthangtik15/tracking/"+ x+ ".png", imgCrop)
+            cv2.imwrite("/home/vietthangtik15/tracking/"+ str(x) + ".png", imgCrop)
             imgCrop = self.transform(Image.fromarray(imgCrop).convert("RGB"))
             imgCrop = imgCrop.unsqueeze(0)
             imgCrops.append(imgCrop)
