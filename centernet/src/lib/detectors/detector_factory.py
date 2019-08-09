7c31614d7f6a542bb93b5077ed0064b554796562
VietThang <vietthangtik15@gmail.com>
2019-08-09 10:18:19 +0700

    test centernet

--- centernet/src/lib/detectors/detector_factory.py
+++ centernet/src/lib/detectors/detector_factory.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .exdet import ExdetDetector
+# from .exdet import ExdetDetector
 from .ddd import DddDetector
 from .ctdet import CtdetDetector
 from .multi_pose import MultiPoseDetector
