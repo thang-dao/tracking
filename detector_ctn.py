4509d6e2470194fa439143565694a2ecc9b8ada0
VietThang <vietthangtik15@gmail.com>
2019-08-09 10:22:56 +0700

    test centernet

--- detector_ctn.py
+++ detector_ctn.py
@@ -8,7 +8,7 @@ from opts import opts
 MODEL_PATH = 'centernet/models/ctdet_coco_dla_2x.pth'
 TASK = 'ctdet'
 opt = opts().init('{} --load_model {}'.format(TASK, MODEL_PATH).split(' '))
-detector = detector.detector_factory[opt.task](opt)
+detector = detector_factory[opt.task](opt)
 
 
 # video = '/home/viettthangtik15/dataset/input/video_1.mp4'
