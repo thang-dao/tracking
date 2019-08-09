e264eb001c17d29d902172c53eda6a7c51f32c57
VietThang <vietthangtik15@gmail.com>
2019-08-09 10:58:49 +0700

    test centernet

--- detector_ctn.py
+++ detector_ctn.py
@@ -15,9 +15,14 @@ detector = detector_factory[opt.task](opt)
 # video = '/home/viettthangtik15/dataset/input/video_1.mp4'
 img = 'centernet/images/17790319373_bd19b24cfc_k.jpg'
 ret = detector.run(img)
-for key, value in ret.items():
-	x1, y1, x2, y2 = ret['results']
+for key, value in ret['results'].items():
+	id = key
+	x1 = value[0]
+	y1 = value[1] 
+	x2 = value[2]
+	y2 = value[3] 
+	score = value[4]
 	print(x1, y1, x2, y2)
-	imgcrop = img[int(y1):int(y2), int(x1):int(x2)]
-	cv2.imwrite('/home/dataset/output/' + str(x1) +'jpg', imgcrop)
-cv2.destroyAllWindows()
+	# imgcrop = img[int(y1):int(y2), int(x1):int(x2)]
+	# cv2.imwrite('/home/dataset/output/' + str(x1) +'jpg', imgcrop)
+# cv2.destroyAllWindows()
