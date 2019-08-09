253ffac8443e46e8450d10910acc33551f4317e3
VietThang <vietthangtik15@gmail.com>
2019-08-09 13:34:51 +0700

    fix centernet

--- detector_ctn.py
+++ detector_ctn.py
@@ -2,7 +2,6 @@ import sys
 import cv2
 CENTERNET_PATH = '/home/vietthangtik15/tracking/centernet/src/lib/'
 sys.path.insert(0, CENTERNET_PATH)
-# import _init_paths
 from detectors.detector_factory import detector_factory
 from opts import opts 
 
@@ -16,16 +15,17 @@ detector = detector_factory[opt.task](opt)
 image = 'centernet/images/17790319373_bd19b24cfc_k.jpg'
 img = cv2.imread(image, 1)
 ret = detector.run(img)
-for key, value in ret['results'].items():
-	for imcp in value:
-		ids = key
-		x1 = imcp[0]
-		y1 = imcp[1] 
-		x2 = imcp[2]
-		y2 = imcp[3] 
-		score = imcp[4]
-		print(ids)
-		if score > 0.3 and ids == 1:
-			imgcrop = img[int(y1):int(y2), int(x1):int(x2)]		
-			cv2.imwrite('/home/vietthangtik15/dataset/output/' + str(x1) +'.jpg', imgcrop)
+
+# for key, value in ret['results'].items():
+# 	for imcp in value:
+# 		ids = key
+# 		x1 = imcp[0]
+# 		y1 = imcp[1] 
+# 		x2 = imcp[2]
+# 		y2 = imcp[3] 
+# 		score = imcp[4]
+# 		if score > 0.3 and ids == 1:
+# 			imgcrop = img[int(y1):int(y2), int(x1):int(x2)]		
+# 			cv2.imwrite('/home/vietthangtik15/dataset/output/' + str(x1) +'.jpg', imgcrop)
+print(len(ret['results'][1]))
 cv2.destroyAllWindows()
