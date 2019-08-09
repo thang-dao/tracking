f4aad33712b8f2ae3cb768a1d006e753164cc4d1
VietThang <vietthangtik15@gmail.com>
2019-08-09 14:59:08 +0700

    tracking by centernet-deepsort

--- detector_ctn.py
+++ detector_ctn.py
@@ -1,5 +1,6 @@
 import sys
 import cv2
+import glob
 CENTERNET_PATH = '/home/vietthangtik15/tracking/centernet/src/lib/'
 sys.path.insert(0, CENTERNET_PATH)
 from detectors.detector_factory import detector_factory
@@ -10,24 +11,23 @@ TASK = 'ctdet'
 opt = opts().init('{} --load_model {}'.format(TASK, MODEL_PATH).split(' '))
 detector = detector_factory[opt.task](opt)
 
-
 # video = '/home/viettthangtik15/dataset/input/video_1.mp4'
-image = '/home/vietthangtik15/dataset/input/ori_im.jpg'
-img = cv2.imread(image, 1)
-ret = detector.run(img)
-count = 0
-for key, value in ret['results'].items():
-	for imcp in value:
-		ids = key
-		x1 = imcp[0]
-		y1 = imcp[1] 
-		x2 = imcp[2]
-		y2 = imcp[3] 
-		score = imcp[4]
-		if ids == 1:
-			count += 1
-			print(x1, y1, x2, y2)
-			imgcrop = img[int(y1):int(y2), int(x1):int(x2)]		
-			cv2.imwrite('/home/vietthangtik15/dataset/output/' + str(x1) +'.jpg', imgcrop)
-print(len(ret['results'][1]), count)
+dirs = '/home/vietthangtik15/dataset/input/*.jpg'
+for img in glob.glob(dirs):
+	img = cv2.imread(img, 1)
+	ret = detector.run(img)
+	count = 0
+	for key, value in ret['results'].items():
+		for imcp in value:
+			ids = key
+			x1 = imcp[0]
+			y1 = imcp[1] 
+			x2 = imcp[2]
+			y2 = imcp[3] 
+			score = imcp[4]
+			if ids == 1:
+				count += 1
+				imgcrop = img[int(y1):int(y2), int(x1):int(x2)]		
+				cv2.imwrite('/home/vietthangtik15/dataset/output/' + str(count) +'.jpg', imgcrop)
+	print(len(ret['results'][1]), count)
 cv2.destroyAllWindows()
