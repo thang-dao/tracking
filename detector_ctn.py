6caa377d077dba91dc02b8c3e077c2f6e31fea2e
VietThang <vietthangtik15@gmail.com>
2019-08-09 09:38:01 +0700

    test centernet

---
+++ detector_ctn.py
@@ -0,0 +1,15 @@
+import sys
+CENTERNET_PATH = '~/centernet/src/lib/'
+sys.path.insert(0, CENTERNET_PATH)
+from centernet.src.lib.detector.detector_factory import dectector_factory
+from opts import opts
+MODEL_PATH = '~/centernet/models/ctdet_coco_dla_2x.pth'
+TASK = 'ctdet'
+opt = opts().init('{} --load_model'.format(TASK, MODEL_PATH).split(' '))
+detector = detector.detector_factory[opt.task](opt)
+
+
+video = '/home/viettthangtik15/dataset/input/video_1.mp4'
+ret = detector.run(video)['category_id']
+print(ret)
+
