4668a008d4adae4352eb52d8d1d77f37566dd4ab
VietThang <vietthangtik15@gmail.com>
2019-08-09 10:01:56 +0700

    test centernet

--- detector_ctn.py
+++ detector_ctn.py
@@ -1,9 +1,9 @@
 import sys
-CENTERNET_PATH = '/home/vietthangtik15/tracking/centernet/src/lib/'
-sys.path.insert(0, CENTERNET_PATH)
-from detectors.detector_factory import detector_factory
+# CENTERNET_PATH = '/home/vietthangtik15/tracking/centernet/src/lib/'
+# sys.path.insert(0, CENTERNET_PATH)
+from centernet.src.lib.detectors.detector_factory import detector_factory
 from opts import opts 
-MODEL_PATH = '~/centernet/models/ctdet_coco_dla_2x.pth'
+MODEL_PATH = 'centernet/models/ctdet_coco_dla_2x.pth'
 TASK = 'ctdet'
 opt = opts().init('{} --load_model'.format(TASK, MODEL_PATH).split(' '))
 detector = detector.detector_factory[opt.task](opt)
