696b36e5a74a4a4a827f0e148ef1a02439aa9e08
VietThang <vietthangtik15@gmail.com>
2019-08-09 10:33:51 +0700

    test centernet

--- detector_ctn.py
+++ detector_ctn.py
@@ -14,6 +14,5 @@ detector = detector_factory[opt.task](opt)
 # video = '/home/viettthangtik15/dataset/input/video_1.mp4'
 img = 'centernet/images/16004479832_a748d55f21_k.jpg'
 ret = detector.run(img)
-for key, value in ret.items():
+for key, value in ret['results'].items():
 	print(key)
-print(type(ret['results']))
