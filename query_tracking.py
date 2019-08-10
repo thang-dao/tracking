000cee31505446f2627a511a37db8528d5da1f43
VietThang <vietthangtik15@gmail.com>
2019-08-10 10:34:06 +0700

    fix detection

--- query_tracking.py
+++ query_tracking.py
@@ -78,13 +78,15 @@ class Detector(object):
 
                 ret = self.centernet.run(ori_im)
                 confidences = []
+                bbox = []
                 if ret['results'] is not None:
                     for box in ret['results'][1]:
                         for i in range(4):
                             if box[i] < 0:
                                 box[i] = 0 
                         confidences.append(box[4])
-                    del ret['results'][1][4]
+                        box.remove(box[4])
+                        bbox.append(box)
                     outputs = self.deepsort.update(ret['results'][1], confidences, im)
                     if len(outputs) > 0:
                         bbox_xyxy = outputs[:,:4]
