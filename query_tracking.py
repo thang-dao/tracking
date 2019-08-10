734e2a16d4a85d543dc4970a80807a3c870a71a1
VietThang <vietthangtik15@gmail.com>
2019-08-10 10:49:46 +0700

    fix detection

--- query_tracking.py
+++ query_tracking.py
@@ -78,7 +78,7 @@ class Detector(object):
 
                 ret = self.centernet.run(ori_im)
                 confidences = []
-                bbox = np.zeros_like(ret['results'][1])
+                bbox = []
                 if ret['results'] is not None:
                     for box in ret['results'][1]:
                         for i in range(4):
